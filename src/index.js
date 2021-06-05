7b52dd0e667de69b420d80a70f37386ccbf8dea2
markmead <itsmarkmead@gmail.com>
2021-06-05 19:43:50 +0100

    clean up code

--- src/index.js
+++ src/index.js
@@ -2,25 +2,20 @@ export function render(type, url) {
   const els = [...document.querySelectorAll(`[data-render=${type}]`)]
 
   for (const el of els) {
-    const dataset = el.dataset
+    const data = el.dataset
 
     fetch(url, {
       method: 'get',
     })
       .then((res) => res.text())
       .then((html) => {
-        const datakeys = Object.keys(dataset)
+        const keys = Object.keys(data)
 
-        for (const key of datakeys) {
-          html = html.replace(`{{${key}}}`, dataset[key])
+        for (const key of keys) {
+          html = html.replace(`{{${key}}}`, data[key])
         }
 
         el.innerHTML = html
       })
   }
 }
-
-window.addEventListener('load', function () {
-  render('card', 'card.html')
-  render('banner', 'banner.html')
-})
