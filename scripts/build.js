7b7434345d27f8b2fa9758d5426129fc24a234e9
Mark Mead <itsmarkmead@gmail.com>
2022-07-25 08:36:04 +0100

    Fix - Name

--- scripts/build.js
+++ scripts/build.js
@@ -1,28 +1,28 @@
 build({
-    entryPoints: [`builds/cdn.js`],
-    outfile: `dist/assistant.min.js`,
-    platform: 'browser',
-    define: { CDN: true },
-  })
+  entryPoints: [`builds/cdn.js`],
+  outfile: `dist/render.min.js`,
+  platform: 'browser',
+  define: { CDN: true },
+})
 
-  build({
-    entryPoints: [`builds/module.js`],
-    outfile: `dist/assistant.esm.js`,
-    platform: 'neutral',
-    mainFields: ['main', 'module'],
-  })
+build({
+  entryPoints: [`builds/module.js`],
+  outfile: `dist/render.esm.js`,
+  platform: 'neutral',
+  mainFields: ['main', 'module'],
+})
 
-  build({
-    entryPoints: [`builds/module.js`],
-    outfile: `dist/assistant.cjs.js`,
-    target: ['node10.4'],
-    platform: 'node',
-  })
+build({
+  entryPoints: [`builds/module.js`],
+  outfile: `dist/render.cjs.js`,
+  target: ['node10.4'],
+  platform: 'node',
+})
 
-  function build(options) {
-    options.define || (options.define = {})
+function build(options) {
+  options.define || (options.define = {})
 
-    return require('esbuild')
-      .build({ ...options, minify: true, bundle: true })
-      .catch(() => process.exit(1))
-  }
+  return require('esbuild')
+    .build({ ...options, minify: true, bundle: true })
+    .catch(() => process.exit(1))
+}
